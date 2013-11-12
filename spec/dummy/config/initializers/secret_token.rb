563e065c6a59b2547ba03624498dcf8af10a5b5f
maeve <maeve.revels@getg5.com>
2013-11-12 21:51:03 +0000

    Generate dummy Rails app for integration testing

---
+++ spec/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '647a56085c4f52cc5e906029f424ff1fa1d118d66800fb131b28c7ef7467f379f253ec847cc15af0036a8c55a4c547c9ef73d09d17bf00f9ecbd6823e90edbae'
