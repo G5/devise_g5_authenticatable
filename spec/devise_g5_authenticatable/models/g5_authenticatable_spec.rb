a8bff26f343644041452c4256580c90d8a5e53c3
maeve <maeve.revels@getg5.com>
2014-01-03 19:24:44 +0000

    Add email column back to the users table
    Existing code in reputation expects email to be a column in AR.
    Ideally, the extension will support both ways, but that's not a
    priority.

--- spec/devise_g5_authenticatable/models/g5_authenticatable_spec.rb
+++ spec/devise_g5_authenticatable/models/g5_authenticatable_spec.rb
@@ -5,7 +5,13 @@ describe Devise::Models::G5Authenticatable do
 
   let(:model) { User.new }
 
-  it 'should allow mass assignment of email' do
-    expect(model).to allow_mass_assignment_of(:email)
+  it { should allow_mass_assignment_of(:email) }
+
+  it 'should persist the email' do
+    new_email = 'new.email@test.host'
+    model.email = new_email
+    model.save!
+    model.reload
+    expect(model.email).to eq(new_email)
   end
 end
