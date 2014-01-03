a37d2ac75ee0ebb39163e739a571a80166ad6b77
maeve <maeve.revels@getg5.com>
2014-01-03 18:15:09 +0000

    Add omniauth columns to the User model in the dummy app

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140103042329) do
+ActiveRecord::Schema.define(:version => 20140103174810) do
 
   create_table "users", :force => true do |t|
     t.integer  "sign_in_count",      :default => 0, :null => false
@@ -21,6 +21,11 @@ ActiveRecord::Schema.define(:version => 20140103042329) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                        :null => false
     t.datetime "updated_at",                        :null => false
+    t.string   "uid"
+    t.string   "provider"
+    t.string   "g5_access_token"
   end
+
+  add_index "users", ["provider", "uid"], :name => "index_users_on_provider_and_uid", :unique => true
 
 end
