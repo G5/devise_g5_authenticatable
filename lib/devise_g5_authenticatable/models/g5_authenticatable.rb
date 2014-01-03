1f9c4f27304ea02ce42aa206fa8665e02e544ca0
maeve <maeve.revels@getg5.com>
2014-01-03 20:52:31 +0000

    Start documenting the model mixin

--- lib/devise_g5_authenticatable/models/g5_authenticatable.rb
+++ lib/devise_g5_authenticatable/models/g5_authenticatable.rb
@@ -1,5 +1,13 @@
 module Devise
   module Models
+    # Authenticatable module, responsible for remote credential management
+    # in G5 Auth.
+    #
+    # The module assumes that the following attributes have already been defined
+    # on the model:
+    #   * `provider`: the value will always be 'g5' for G5 Auth users
+    #   * `uid`: the unique id for this user in G5 Auth
+    #   * `g5_access_token`: the current OAuth access token, if one exists
     module G5Authenticatable
       extend ActiveSupport::Concern
 
