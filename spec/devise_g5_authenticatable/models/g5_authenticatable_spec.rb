360b653536ae09ac8817696d62c47e7dfed9a8b1
maeve <maeve.revels@getg5.com>
2014-01-04 19:48:11 -0800

    Move specs for User#update_with_password from reputation

--- spec/devise_g5_authenticatable/models/g5_authenticatable_spec.rb
+++ spec/devise_g5_authenticatable/models/g5_authenticatable_spec.rb
@@ -55,9 +55,154 @@ describe Devise::Models::G5Authenticatable do
       end
 
       it 'should not persist updated by' do
-        model.updated_by = User.new
         save_model
         expect(model_class.find(model.id).updated_by).to be_nil
+      end
+    end
+  end
+
+  describe '#update_with_password' do
+    subject { update_with_password }
+    let(:update_with_password) { model.update_with_password(params) }
+
+    let(:model) { create(:user) }
+
+    let(:params) do
+      {current_password: current_password,
+       password: updated_password,
+       password_confirmation: updated_password,
+       email: updated_email}
+    end
+
+    let(:current_password) {}
+    let(:updated_password) { 'updated_secret' }
+    let(:updated_email) { 'update@email.com' }
+
+    let(:auth_updater) { double(:auth_user_updater, update: true) }
+    before { allow(AuthUserUpdater).to receive(:new).and_return(auth_updater) }
+
+    let(:password_validator) { double(:auth_password_validator) }
+    before do
+      allow(AuthPasswordValidator).to receive(:new).
+        and_return(password_validator)
+    end
+
+    context 'with valid current password' do
+      before { allow(password_validator).to receive(:valid_password?).and_return(true) }
+
+      context 'with updated password' do
+        context 'with valid input' do
+          it 'should return true' do
+            expect(update_with_password).to be_true
+          end
+
+          it 'should clear the user password' do
+            expect { update_with_password }.to change { model.password }.to(nil)
+          end
+
+          it 'should clear the user password_confirmation' do
+            expect { update_with_password }.to change { model.password_confirmation }.to(nil)
+          end
+
+          it 'should update the credentials in the auth server' do
+            expect(auth_updater).to receive(:update).with(model)
+            update_with_password
+          end
+
+          it 'should update the email on the local model' do
+            expect { update_with_password }.to change { model.email }.to(updated_email)
+          end
+        end
+
+        context 'with invalid email' do
+          let(:updated_email) { '' }
+
+          it 'should return false' do
+            expect(update_with_password).to be_false
+          end
+
+          it 'should not update the credentials on the auth server' do
+            expect(auth_updater).to_not receive(:update)
+            update_with_password
+          end
+
+          it 'should add an error to the email attribute' do
+            expect { update_with_password }.to change { model.errors[:email].count }.to(1)
+          end
+        end
+      end
+
+      context 'without updated password' do
+        let(:updated_password) { '' }
+        before { model.clean_up_passwords }
+
+        context 'with updated email' do
+          it 'should return true' do
+            expect(update_with_password).to be_true
+          end
+
+          it 'should update the credentials on the auth server' do
+            expect(auth_updater).to receive(:update).with(model)
+            update_with_password
+          end
+
+          it 'should update the email on the local model' do
+            expect { update_with_password }.to change { model.email }.to(updated_email)
+          end
+        end
+
+        context 'with unchanged email' do
+          let(:updated_email) { model.email }
+
+          it 'should return true' do
+            expect(update_with_password).to be_true
+          end
+
+          it 'should not update the credentials in the auth server' do
+            expect(auth_updater).to_not receive(:update)
+            update_with_password
+          end
+        end
+      end
+    end
+
+    context 'with invalid current password' do
+      before { allow(password_validator).to receive(:valid_password?).and_return(false) }
+
+      context 'when current password is missing' do
+        let(:current_password) { '' }
+
+        it 'should return false' do
+          expect(update_with_password).to be_false
+        end
+
+        it 'should set an error on the current_password attribute' do
+          update_with_password
+          expect(model.errors[:current_password]).to include("can't be blank")
+        end
+
+        it 'should not update user credentials in the remote server' do
+          expect(auth_updater).to_not receive(:update)
+          update_with_password
+        end
+      end
+
+      context 'when current password is incorrect' do
+        let(:current_password) { 'something wrong' }
+
+        it 'should return false' do
+          expect(update_with_password).to be_false
+        end
+
+        it 'should set an error on the current_password attribute' do
+          update_with_password
+          expect(model.errors[:current_password]).to include('is invalid')
+        end
+
+        it 'should not update user credentials in the remote server' do
+          expect(auth_updater).to_not receive(:update)
+          update_with_password
+        end
       end
     end
   end
--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,7 +1,8 @@
 FactoryGirl.define do
   factory :user do
     sequence(:email) { |n| "user.#{n}@test.host" }
-    password 'my_dark_secret'
+    password 'my_new_secret'
+    password_confirmation 'my_new_secret'
     provider 'g5'
     sequence(:uid) { |n| "remote-user-#{n}" }
   end
