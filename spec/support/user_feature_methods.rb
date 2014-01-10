1959bc53b73d6de209f250ecb6eba411b6bf90ac
maeve <maeve.revels@getg5.com>
2014-01-10 19:57:01 +0000

    Change feature specs to use OmniAuth test mode

--- spec/support/user_feature_methods.rb
+++ spec/support/user_feature_methods.rb
@@ -1,12 +1,24 @@
 module UserFeatureMethods
+  def stub_g5_omniauth(user, options={})
+    OmniAuth.config.mock_auth[:g5] = OmniAuth::AuthHash.new({
+      uid: user.uid,
+      provider: 'g5',
+      info: {email: user.email},
+      credentials: {token: user.g5_access_token}
+    }.merge(options))
+  end
+
+  def stub_g5_invalid_credentials
+    OmniAuth.config.mock_auth[:g5] = :invalid_credentials
+  end
+
   def visit_path_and_login_with(path, user)
-    login_as user
+    stub_g5_omniauth(user)
     visit path
   end
 end
 
 RSpec.configure do |config|
-  config.include Warden::Test::Helpers, type: :feature
-  config.after(:each, type: :feature) { Warden.test_reset! }
+  config.before(:each) { OmniAuth.config.test_mode = true }
   config.include UserFeatureMethods, type: :feature
 end
