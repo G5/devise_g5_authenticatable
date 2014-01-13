c56475e462f4c618092fc85e43697421b6e0985a
maeve <maeve.revels@getg5.com>
2014-01-13 22:27:57 +0000

    Move model mixin

--- lib/devise_g5_authenticatable.rb
+++ lib/devise_g5_authenticatable.rb
@@ -8,4 +8,4 @@ require 'devise_g5_authenticatable/controllers/url_helpers'
 Devise.add_module(:g5_authenticatable,
                   strategy: false,
                   route: { session: [:new, :destroy] },
-                  model: 'devise_g5_authenticatable/models/g5_authenticatable')
+                  model: 'devise_g5_authenticatable/model')
