e41e293386a729dae47b6e7cd5bde16306411983
maeve <maeve.revels@getg5.com>
2014-01-17 03:15:58 +0000

    Add omniauth config to devise initializer in dummy app

--- spec/dummy/config/initializers/devise.rb
+++ spec/dummy/config/initializers/devise.rb
@@ -231,7 +231,8 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  # config.omniauth :github, 'APP_ID', 'APP_SECRET', :scope => 'user,public_repo'
+  config.omniauth :g5, ENV['G5_AUTH_CLIENT_ID'], ENV['G5_AUTH_CLIENT_SECRET'],
+                  client_options: {site: ENV['G5_AUTH_ENDPOINT']}
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
