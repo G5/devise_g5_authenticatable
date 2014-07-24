074c93773bd10ac0cea9189e0bbf2517235cc8b8
maeve <maeve.revels@getg5.com>
2014-07-24 01:03:56 +0000

    Update js compression options

--- spec/dummy/config/environments/development.rb
+++ spec/dummy/config/environments/development.rb
@@ -26,7 +26,7 @@ Dummy::Application.configure do
   config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   # Do not compress assets
-  config.assets.compress = false
+  config.assets.js_compressor = nil
 
   # Expands the lines which load the assets
   config.assets.debug = true
