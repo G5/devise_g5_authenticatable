357773b1f806a3c77341a52a67ebe784d2d1bbc7
maeve <maeve.revels@getg5.com>
2015-08-21 01:07:39 +0000

    Remove rake g5:export_users

--- lib/devise_g5_authenticatable/engine.rb
+++ lib/devise_g5_authenticatable/engine.rb
@@ -3,9 +3,5 @@ module DeviseG5Authenticatable
     initializer "devise_g5_authenticatable.helpers" do
       Devise.include_helpers(DeviseG5Authenticatable)
     end
-
-    rake_tasks do
-      load 'tasks/g5/export_users.rake'
-    end
   end
 end
--- lib/devise_g5_authenticatable/g5.rb
+++ lib/devise_g5_authenticatable/g5.rb
@@ -1,4 +1,3 @@
 require 'devise_g5_authenticatable/g5/auth_user_creator'
 require 'devise_g5_authenticatable/g5/auth_user_updater'
 require 'devise_g5_authenticatable/g5/auth_password_validator'
-require 'devise_g5_authenticatable/g5/user_exporter'
